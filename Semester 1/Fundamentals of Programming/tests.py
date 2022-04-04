1f71e37f028cedc66dce3ffbaf1160db284016d1
917_Truta_Dan <65197215+trutadan@users.noreply.github.com>
2022-04-05 00:34:41 +0300

    a678 successfully created...

--- Semester 1/Fundamentals of Programming/tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from domain.testEntities import *
-from domain.testValidators import *
-from repository.testRepositories import *
-from services.testServices import *
-
-
-if __name__ == "__main__":
-    unittest.main()
