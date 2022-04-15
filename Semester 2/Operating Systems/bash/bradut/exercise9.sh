94eb5cdbf95f0e4808f4e1a43f224ba05b875897
Dan <65197215+trutadan@users.noreply.github.com>
2022-04-15 11:04:43 +0300

    bradut exercises successfully created...

---
+++ Semester 2/Operating Systems/bash/bradut/exercise9.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+        echo "Not enough arguments."
+        exit 1
+fi
+
+while [[ $# -gt 0 ]]; do
+        file=$1
+        if test -f $file; then
+                message=$(cat $file | awk '{
+                for(i=1;i<=NF;i++)
+                        if ($i ~ /^[0-9]*[02468]$/)
+                                freq[$i]++;
+                }
+                END{
+                        for (elem in freq) 
+                                print "Number " elem " has frequency " freq[elem]}
+                                                                                ')
+                echo "$message"
+        else
+                echo "The $1 argument is not a file"    
+        fi
+
+        shift
+done
