e6a0477249c7af3cdc56b286a4ac7e2be903d617
Dan <65197215+trutadan@users.noreply.github.com>
2022-10-13 23:29:01 +0300

    e01 has been successfully created...

--- Semester 3/Computer Networks/Lab02/e01/client.c
+++ Semester 3/Computer Networks/Lab02/e01/client.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char** argv) {
        if (argc < 2) {
-              printf("./<executable> <IPv4>");
+              printf("./<executable> <IPv4>\n");
               return 1;
        }
        
