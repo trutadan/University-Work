65b4583ee63b8e76bbe8d40d1acf9f29eb7d7541
Dan <65197215+trutadan@users.noreply.github.com>
2022-11-15 16:53:54 +0200

    Lab02 has been successfully created...

--- Semester 3/Advanced Programming Methods/Lab02/Model/Types/BoolType.java
+++ Semester 3/Advanced Programming Methods/Lab02/Model/Types/BoolType.java
@@ -5,7 +5,7 @@ import Model.Values.BoolValue;
 // done for LAB02
 public class BoolType implements Type {
     public boolean equals(Object another) {
-        return another instanceof IntType;
+        return another instanceof BoolType;
     }
     public BoolValue getDefaultValue() {
         return new BoolValue(false);
