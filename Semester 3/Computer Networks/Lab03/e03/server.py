8735278152aff7dbcb0619a95de93f4a09100acb
Dan <65197215+trutadan@users.noreply.github.com>
2022-11-16 09:29:42 +0200

    e03 has been successfully created...

--- Semester 3/Computer Networks/Lab03/e03/server.py
+++ Semester 3/Computer Networks/Lab03/e03/server.py
@@ -4,13 +4,12 @@ import select
 
 
 def send_clients(clients):
-    data_to_send = ''
-    for client_information in clients.values():
-        data_to_send += client_information[0] + ',' + str(client_information[1]) + ';'
-        
-    data_to_send = data_to_send[:-1]
+    send_data = ''
+    for info in clients.values():
+        send_data += info[0]+','+str(info[1])+';'
+    send_data = send_data[:-1]
     for client in clients:
-        client.send(data_to_send.encode())
+        client.send(send_data.encode())
 
 
 if __name__ == '__main__':
@@ -18,28 +17,26 @@ if __name__ == '__main__':
                     # values: (udp_port - udp communication happens here, ip address - communication)
     rdv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     rdv.bind(('127.0.0.1', 1234))
-    rdv.listen(5)
+    rdv.listen(7)
 
-    reading_sockets = [rdv]
+    read_sock = [rdv]
     while True:
-        sockets_ready_for_read, _, _ = select.select(reading_sockets, [], [])
-        for socket in sockets_ready_for_read:
-            if socket is rdv:
+        ready_read, _, _ = select.select(read_sock, [], [])
+        for s in ready_read:
+            if s is rdv:
                 # we have a new client connection
-                client_connection, client_address = socket.accept()
-                print('New client from the address: ', client_address)
-                
-                c_udpp = client_connection.recv(4)
+                cs, addr = s.accept()
+                print('new client from addr: ', addr)
+                c_udpp = cs.recv(4)
                 c_udpp = struct.unpack('!I', c_udpp)[0]
-                clients[client_connection] = (client_address[0], c_udpp)
-                
+                clients[cs] = (addr[0], c_udpp)
                 send_clients(clients)
-                reading_sockets.append(client_connection)
-                
+                read_sock.append(cs)
             else:
-                data = socket.recv(512)
+                data = s.recv(512)
                 if not data or 'quit' in data.decode().lower():
-                    socket.close()
-                    clients.pop(socket)
-                    reading_sockets.remove(socket)
+                    s.close()
+                    clients.pop(s)
+                    read_sock.remove(s)
                     send_clients(clients)
+
