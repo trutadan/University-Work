2f2931588709bbcc15e5b736097d3c4d722840f4
Dan <65197215+trutadan@users.noreply.github.com>
2022-11-21 22:11:19 +0200

    e07a has been successfully created...

---
+++ Semester 3/Logical and Functional Programming/P1/e07a.pl
@@ -0,0 +1,13 @@
+% 7. a. Write a predicate to compute the intersection of two sets.
+checkListContainsElement([H|_], H):- !.
+checkListContainsElement([H|T], E):-
+    H =\= E,
+	checkListContainsElement(T, E).
+
+getIntersectionOfSets(_, [], []).
+getIntersectionOfSets(S, [H|T], [H|R]):-
+    checkListContainsElement(S, H), 
+    getIntersectionOfSets(S, T, R), !.
+getIntersectionOfSets(S, [H|T], R):-
+    \+(checkListContainsElement(S, H)),
+    getIntersectionOfSets(S, T, R), !.
