6dca885725ac89015f7a4e1f62f2921a7b9ee270
Dan <65197215+trutadan@users.noreply.github.com>
2022-11-21 22:37:52 +0200

    e11a has been successfully created...

---
+++ Semester 3/Logical and Functional Programming/P2/e11a.pl
@@ -0,0 +1,7 @@
+% 11. a. Replace all occurrences of an element from a list with another element e.
+replaceElementFromListWithAnotherElement([], _, _, []).
+replaceElementFromListWithAnotherElement([H|T], H, NE, [NE|R]):- !,
+	replaceElementFromListWithAnotherElement(T, H, NE, R).
+replaceElementFromListWithAnotherElement([H|T], E, NE, [H|R]):-
+  H =\= E, !,
+	replaceElementFromListWithAnotherElement(T, E, NE, R).
