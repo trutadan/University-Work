14f5764d62e43fe2de23b0666e92987a3d580dcd
Dan <65197215+trutadan@users.noreply.github.com>
2023-10-31 13:02:53 +0200

    updated ProgramInternalForm.java...

--- Semester 5/Formal Languages and Compiler Design/lab03/ProgramInternalForm.java
+++ Semester 5/Formal Languages and Compiler Design/lab03/ProgramInternalForm.java
@@ -2,18 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProgramInternalForm {
-    private final List<Pair<Pair<String, Integer>, Pair<Integer, Integer>>> array;
+    private final List<Pair<String, Pair<Integer, Integer>>> array;
 
     public ProgramInternalForm() {
         this.array = new ArrayList<>();
     }
 
-    public List<Pair<Pair<String, Integer>, Pair<Integer, Integer>>> getArray() {
+    public List<Pair<String, Pair<Integer, Integer>>> getArray() {
         return this.array;
     }
 
-    public void add(Pair<String, Integer> token, Pair<Integer, Integer> symbolTablePosition) {
-        Pair<Pair<String, Integer>, Pair<Integer, Integer>> pair = new Pair<>(token, symbolTablePosition);
+    public void add(String token, Pair<Integer, Integer> symbolTablePosition) {
+        Pair<String, Pair<Integer, Integer>> pair = new Pair<>(token, symbolTablePosition);
         this.array.add(pair);
     }
 
@@ -22,8 +22,8 @@ public class ProgramInternalForm {
         StringBuilder stringRepresentation = new StringBuilder();
         stringRepresentation.append("PIF: {\n");
 
-        for (Pair<Pair<String, Integer>, Pair<Integer, Integer>> pair : this.array)
-            stringRepresentation.append("  (").append(pair.getKey().toString()).append(", ").append(pair.getValue().toString()).append(")\n");
+        for (Pair<String, Pair<Integer, Integer>> pair : this.array)
+            stringRepresentation.append("  (").append(pair.getKey()).append(", ").append(pair.getValue().toString()).append(")\n");
 
         stringRepresentation.append("}");
         return stringRepresentation.toString();
