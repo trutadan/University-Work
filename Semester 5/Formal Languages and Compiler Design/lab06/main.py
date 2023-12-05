34e4005611008a0bd264775e5a287a8c4d4d3346
Dan <65197215+trutadan@users.noreply.github.com>
2023-12-05 03:39:57 +0200

    successfully created lab06...

---
+++ Semester 5/Formal Languages and Compiler Design/lab06/main.py
@@ -0,0 +1,43 @@
+from grammar import Grammar
+from recursive_descentant import RecursiveDescendant
+
+if __name__ == '__main__':
+    filename = "gtest.txt"
+    grammar = Grammar(filename)
+
+    parser = RecursiveDescendant(grammar)
+
+    actions_sequence_example = ("exp adv exp adv exp mi at mi at adv adv exp mi at mi at adv mi back at back back at "
+                                "back at adv exp mi at mi at adv adv exp mi at mi at adv success")
+
+    actions = actions_sequence_example.split()
+
+    print("Parsing the sequence w = aacbc...")
+    print(parser)
+    for index in range(len(actions)):
+        if actions[index] == "exp":
+            parser.expand()
+        elif actions[index] == "adv":
+            parser.advance()
+        elif actions[index] == "mi":
+            parser.momentary_insuccess()
+        elif actions[index] == "back":
+            parser.back()
+        elif actions[index] == "at":
+            parser.another_try()
+        elif actions[index] == "success":
+            parser.success()
+        else:
+            print("error")
+
+    parser = RecursiveDescendant(grammar)
+
+    print("\nParsing the sequence w = b...")
+    print(parser)
+    parser.expand()
+    parser.momentary_insuccess()
+    parser.another_try()
+    parser.momentary_insuccess()
+    parser.another_try()
+    parser.momentary_insuccess()
+    parser.another_try()
